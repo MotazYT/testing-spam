ae33bc1c7e7489c8fd53239e28ec3ef6b39283a8
MotazYT <44732876+MotazYT@users.noreply.github.com>
2018-12-16 17:23:58 +0200

    Add files via upload

---
+++ bot (1).js
@@ -0,0 +1,22 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(Logged in as ${client.user.tag}!);  
+});
+
+client.on('ready', async() => {
+var server = "514500934009618433"; // ايدي السررفر
+var channel = "514500934529974303";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , 
+Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , 
+Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , 
+')
+    },305);
+})
+
+client.login(توكن حسابك);
+
+
+ 
